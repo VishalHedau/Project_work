1011dd320277a7f787bb324f4cddf8535823438f
Vishal Hedau <vishalhedau3069@gmail.com>
2023-05-29 11:32:46 +0530

    html and css div tags

--- cms/src/main/java/com/mkpits/cms/repo/StudentRepo.java
+++ cms/src/main/java/com/mkpits/cms/repo/StudentRepo.java
@@ -4,4 +4,9 @@ import com.mkpits.cms.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepo extends JpaRepository<Student, Long> {
+
+
+
+
+
 }
